c1afaf1cae132b182add2dae080ee626371dd3ff
Dan Sparks <dans595@gmail.com>
2019-10-26 10:58:33 -0700

    Add Unlistings signal

---
+++ signal/unlistings/unlistings.go
@@ -0,0 +1,39 @@
+package unlistings
+
+import (
+	"time"
+
+	"github.com/Clever/go-utils/stringset"
+	"github.com/d-sparks/gravy/db"
+	"github.com/d-sparks/gravy/db/dailywindow"
+	"github.com/d-sparks/gravy/signal"
+)
+
+const Name = "unlistings"
+
+// Unlistings reports new values since last tick.
+type Unlistings struct {
+	previousSymbols stringset.StringSet
+}
+
+func New() Unlistings {
+	return signal.NewCachedSignal(
+		Unlistings{previousSymbols: stringset.StringSet{}},
+		time.Hour*24*365,
+	)
+}
+
+func (u *Unlistings) Compute(date time.Time, stores map[string]db.Store) signal.SignalOutput {
+	symbols := stores[dailywindow.Name].Get(date).Window.Symbols
+	output := signal.SignalOutput{StringSet: u.previousSymbols.Minus(symbols)}
+	u.previousSymbols = symbols
+	return output
+}
+
+func (u *Unlistings) Headers() []string {
+	return []string{}
+}
+
+func (u *Unlistings) Debug() map[string]string {
+	return map[string]string{}
+}
