8b99a011d43db64a5583eb3a1bd3b571190b501a
Dan Sparks <dans595@gmail.com>
2019-10-26 12:35:48 -0700

    Compute symbols for each window in InMemoryStore parse

--- db/dailywindow/dailywindow.go
+++ db/dailywindow/dailywindow.go
@@ -2,8 +2,10 @@ package dailywindow
 
 import (
 	"encoding/json"
+	"log"
 	"time"
 
+	"github.com/Clever/go-utils/stringset"
 	"github.com/d-sparks/gravy/db"
 	"github.com/d-sparks/gravy/gravyutil"
 	"github.com/d-sparks/gravy/trading"
@@ -19,13 +21,20 @@ type InMemoryStore struct {
 func NewInMemoryStore(filename string) *InMemoryStore {
 	store := InMemoryStore{data: map[time.Time]*trading.Window{}}
 	scanner := gravyutil.FileScannerOrDie(filename)
+	log.Println("Beginning to load in memory dailywindow store...")
 	for scanner.Scan() {
 		window := trading.Window{}
 		gravyutil.FatalIfErr(json.Unmarshal(scanner.Bytes(), &window))
+		// TODO(dansparks): Maybe serialize these in the csv.
+		window.Symbols = stringset.New()
+		for symbol, _ := range window.Open {
+			window.Symbols.Add(symbol)
+		}
 		store.data[window.Begin] = &window
 		store.dates = append(store.dates, window.Begin)
 	}
 	gravyutil.FatalIfErr(scanner.Err())
+	log.Printf("Loaded %d dailywindows into memory...", len(store.dates))
 	return &store
 }
 
