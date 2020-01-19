832ed0220cca2408f3088f6e4a00d0889b011ae7
Dan Sparks <dans595@gmail.com>
2020-01-18 22:09:46 -0800

    Update uniform strategy and tests

--- strategy/uniform/uniform.go
+++ strategy/uniform/uniform.go
@@ -7,10 +7,12 @@ import (
 	"github.com/d-sparks/gravy/db"
 	"github.com/d-sparks/gravy/db/dailyprices"
 	"github.com/d-sparks/gravy/signal"
+	"github.com/d-sparks/gravy/signal/unlistings"
 	"github.com/d-sparks/gravy/strategy"
 	"github.com/d-sparks/gravy/trading"
 )
 
+// Name of the uniform strategy.
 const Name = "uniform"
 
 // Uniform strategy. Invest equally in all securities.
@@ -18,15 +20,18 @@ type Uniform struct {
 	debug map[string]string
 }
 
+// New Uniform strategy.
 func New() *Uniform {
 	return &Uniform{}
 }
 
+// Name as per the strategy interface.
 func (b *Uniform) Name() string {
 	return Name
 }
 
-// Return a uniform capital distribution every day based on the previous close price.
+// Run the strategy. Always returns a capital distribution on (today's stocks) \bigcap (tomorrow's stocks) that is
+// invested equally in all tickers as today's closing prices.
 func (b *Uniform) Run(
 	date time.Time,
 	stores map[string]db.Store,
@@ -34,26 +39,35 @@ func (b *Uniform) Run(
 ) (*strategy.StrategyOutput, error) {
 	b.debug = map[string]string{}
 
-	// Get yesterday's prices.
+	// Get today's prices.
 	dailyPricesData, err := stores[dailyprices.Name].Get(date)
 	if err != nil {
 		return nil, fmt.Errorf("Error getting daily prices in strategy `%s`: `%s`", Name, err.Error())
 	}
 
-	// Create and return a uniform capital distribution.
-	strategyOutput := strategy.StrategyOutput{
-		CapitalDistribution: trading.NewUniformCapitalDistribution(dailyPricesData.Tickers),
+	// Get unlistings.
+	unlistingsData, err := signals[unlistings.Name].Compute(date, stores)
+	if err != nil {
+		return nil, fmt.Errorf("Error computing unlistings in strategy `%s`: `%s`", Name, err.Error())
 	}
 
-	return &strategyOutput, nil
+	// Create uniform capital distribution.
+	investIn := dailyPricesData.Tickers
+	for ticker := range unlistingsData.StringSet {
+		investIn.Remove(ticker)
+	}
+	uniform := trading.NewUniformCapitalDistribution(investIn)
+	output := strategy.StrategyOutput{CapitalDistribution: uniform}
+
+	return &output, nil
 }
 
-// No data to output
+// Headers as per strategy interface.
 func (b *Uniform) Headers() []string {
 	return []string{}
 }
 
-// Return last run's debug.
+// Debug as per strategy interface.
 func (b *Uniform) Debug() map[string]string {
 	return b.debug
 }
