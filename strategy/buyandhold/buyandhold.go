1a5f157dbf1da982fb1c3f6a86d998e6fd269ab0
Dan Sparks <dans595@gmail.com>
2020-01-18 21:46:26 -0800

    Update buyandhold with unit test

--- strategy/buyandhold/buyandhold.go
+++ strategy/buyandhold/buyandhold.go
@@ -13,26 +13,30 @@ import (
 	"github.com/d-sparks/gravy/trading"
 )
 
+// Name of the buyandhold strategy.
 const Name = "buyandhold"
 
 // BuyAndHold strategy. Invest equally in all securities. When there is an IPO or unlisting.
 type BuyAndHold struct {
 	abstractPortfolio *trading.AbstractPortfolio
+	previousIPOs      bool
 	debug             map[string]string
 }
 
+// New BuyAndHold strategy.
 func New() *BuyAndHold {
-	return &BuyAndHold{}
+	return &BuyAndHold{abstractPortfolio: nil, previousIPOs: false, debug: map[string]string{}}
 }
 
+// Name returns the name as per the strategy interface.
 func (b *BuyAndHold) Name() string {
 	return Name
 }
 
-// Whenever there is an ipo or unlisting, invest equally in all securities. This is achieved by taking a uniform
-// capital distribution and turning it into an abstract portfolio worth $1.0 on the most recent closing prices. This
-// portfolio is memorized until the next ipo or unlisting. On any other day, we imagine having held that original
-// abstract portfolio and recommend a capital distribution that replicates that portfolio at the most recent prices.
+// Run the strategy. In the first run, when there is an unlisting, or the session after an IPO signal, create an
+// abstract portfolio invested uniformly in (tomorrow's tickers) \bigcap (today's tickers) based on today's prices.
+// "Hold" this abstract portfolio by returning a capital distribution that is the distribution of the mature value of
+// the portfolio.
 func (b *BuyAndHold) Run(
 	date time.Time,
 	stores map[string]db.Store,
@@ -40,36 +44,46 @@ func (b *BuyAndHold) Run(
 ) (*strategy.StrategyOutput, error) {
 	b.debug = map[string]string{}
 
-	iposData, err := signals[ipos.Name].Compute(date, stores)
-	if err != nil {
-		return nil, fmt.Errorf("Error computing ipos in strategy `%s`: `%s`", Name, err.Error())
-	}
+	// Get unlistings and prices.
 	unlistingsData, err := signals[unlistings.Name].Compute(date, stores)
 	if err != nil {
 		return nil, fmt.Errorf("Error computing unlistings in strategy `%s`: `%s`", Name, err.Error())
 	}
-	dailyPricesData, err := stores[dailyprices.Name].Get(date)
+	pricesData, err := stores[dailyprices.Name].Get(date)
 	if err != nil {
 		return nil, fmt.Errorf("Error getting daily prices in strategy `%s`: `%s`", Name, err.Error())
 	}
 
-	if b.abstractPortfolio == nil || len(iposData.StringSet) > 0 || len(unlistingsData.StringSet) > 0 {
-		uniform := trading.NewUniformCapitalDistribution(dailyPricesData.Tickers)
-		b.abstractPortfolio = uniform.ToAbstractPortfolioOnClose(dailyPricesData.TickersToPrices, 1.0)
+	// If we need to rebalance our portfolio, do so.
+	if b.abstractPortfolio == nil || b.previousIPOs || len(unlistingsData.StringSet) > 0 {
+		investIn := pricesData.Tickers
+		for ticker := range unlistingsData.StringSet {
+			investIn.Remove(ticker)
+		}
+		uniform := trading.NewUniformCapitalDistribution(investIn)
+		b.abstractPortfolio = uniform.ToAbstractPortfolioOnClose(pricesData.TickersToPrices, 1.0)
 	}
 
+	// If there were IPOs, remember this so we can rebalance in the next session.
+	iposData, err := signals[ipos.Name].Compute(date, stores)
+	if err != nil {
+		return nil, fmt.Errorf("Error computing ipos in strategy `%s`: `%s`", Name, err.Error())
+	}
+	b.previousIPOs = len(iposData.StringSet) > 0
+
+	// Return a capital distribution corresponding to the mature value of the abstract portfolio.
 	strategyOutput := strategy.StrategyOutput{
-		CapitalDistribution: b.abstractPortfolio.ToCapitalDistributionOnClose(dailyPricesData.TickersToPrices),
+		CapitalDistribution: b.abstractPortfolio.ToCapitalDistributionOnClose(pricesData.TickersToPrices),
 	}
 	return &strategyOutput, nil
 }
 
-// No data to output
+// Headers as per the strategy interface.
 func (b *BuyAndHold) Headers() []string {
 	return []string{}
 }
 
-// Return last run's debug.
+// Debug as per the strategy interface.
 func (b *BuyAndHold) Debug() map[string]string {
 	return b.debug
 }
