f362aea40a147438a08b7d2bbf53e9222778c227
Dan Sparks <dans595@gmail.com>
2020-01-18 19:55:36 -0800

    IPOS returns IPOs for NEXT trading day

--- signal/ipos/ipos.go
+++ signal/ipos/ipos.go
@@ -4,45 +4,52 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/Clever/go-utils/stringset"
 	"github.com/d-sparks/gravy/db"
 	"github.com/d-sparks/gravy/db/dailyprices"
 	"github.com/d-sparks/gravy/signal"
 )
 
+// Name of the ipos signal.
 const Name = "ipos"
 
-// IPOs reports tickers not present in the last daily prices.
-type IPOs struct {
-	previousTickers stringset.StringSet
-}
+// IPOs returns symbols which will be be IPOing on the NEXT trading day.
+type IPOs struct{}
 
+// New cached IPOs.
 func New() *signal.CachedSignal {
-	return signal.NewCachedSignal(
-		&IPOs{previousTickers: stringset.StringSet{}},
-		time.Hour*24*365, // one year
-	)
+	return signal.NewCachedSignal(&IPOs{}, time.Hour*24*365)
 }
 
+// Name returns the ipos signal name, for the signal interface.
 func (i *IPOs) Name() string {
 	return Name
 }
 
-// Diffs tickers reported by dailyprices with that seen in the previous computation.
+// Compute computes the signal as per the signal interface.
 func (i *IPOs) Compute(date time.Time, stores map[string]db.Store) (*signal.SignalOutput, error) {
-	dailyPrices, err := stores[dailyprices.Name].Get(date)
+	todayData, err := stores[dailyprices.Name].Get(date)
 	if err != nil {
-		return nil, fmt.Errorf("Error reading dailyprices in `%s`: `%s`", Name, err.Error())
+		return nil, fmt.Errorf("Error reading todayprices in `%s`: `%s`", Name, err.Error())
 	}
-	output := signal.SignalOutput{StringSet: dailyPrices.Tickers.Minus(i.previousTickers)}
-	i.previousTickers = dailyPrices.Tickers
-	return &output, nil
+
+	tomorrowDate, err := stores[dailyprices.Name].NextDate(date)
+	if err != nil {
+		return nil, fmt.Errorf("Error getting next date in `%s`: `%s`", Name, err.Error())
+	}
+
+	tomorrowData, err := stores[dailyprices.Name].Get(*tomorrowDate)
+	if err != nil {
+		return nil, fmt.Errorf("Error getting tomorrowprices in `%s`: `%s`", Name, err.Error())
+	}
+	return &signal.SignalOutput{StringSet: tomorrowData.Tickers.Minus(todayData.Tickers)}, nil
 }
 
+// Headers as per signal interface.
 func (i *IPOs) Headers() []string {
 	return []string{}
 }
 
+// Debug as per signal interface.
 func (i *IPOs) Debug() map[string]string {
 	return map[string]string{}
 }
--- signal/ipos/ipos_test.go
+++ signal/ipos/ipos_test.go
@@ -11,28 +11,31 @@ import (
 )
 
 func TestIPOs(t *testing.T) {
-	yesterdayTime, err := time.Parse("2006-01-02", "2004-08-18")
+	todayTime, err := time.Parse("2006-01-02", "2004-08-18")
 	assert.NoError(t, err)
-	todayTime, err := time.Parse("2006-01-02", "2004-08-19")
+	tomorrowTime, err := time.Parse("2006-01-02", "2004-08-19")
+	assert.NoError(t, err)
+	twoDaysTime, err := time.Parse("2006-01-02", "2004-08-19")
 	assert.NoError(t, err)
 
-	yesterday := db.Data{Tickers: stringset.New("MSFT")}
-	today := db.Data{Tickers: stringset.New("MSFT", "GOOGL")}
+	today := db.Data{Tickers: stringset.New("MSFT")}
+	tomorrow := db.Data{Tickers: stringset.New("MSFT", "GOOGL")}
 
 	testStore := dailyprices.NewInMemoryStore()
-	testStore.Set(yesterdayTime, &yesterday)
 	testStore.Set(todayTime, &today)
+	testStore.Set(tomorrowTime, &tomorrow)
+	testStore.Set(twoDaysTime, &tomorrow)
 
 	stores := map[string]db.Store{}
 	stores[dailyprices.Name] = testStore
 
 	ipos := New()
 
-	yesterdayOutput, err := ipos.Compute(yesterdayTime, stores)
-	assert.NoError(t, err)
-	assert.True(t, yesterdayOutput.StringSet.Equals(stringset.New("MSFT")))
-
 	todayOutput, err := ipos.Compute(todayTime, stores)
 	assert.NoError(t, err)
 	assert.True(t, todayOutput.StringSet.Equals(stringset.New("GOOGL")))
+
+	tomorrowOutput, err := ipos.Compute(tomorrowTime, stores)
+	assert.NoError(t, err)
+	assert.True(t, tomorrowOutput.StringSet.Equals(stringset.New()))
 }
