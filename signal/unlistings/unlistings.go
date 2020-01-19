ae9739ef6cd9aed06b04bdd76d275ff69b421f7d
Dan Sparks <dans595@gmail.com>
2020-01-18 20:02:28 -0800

    Unlistings signal returns tickers unlisting in the NEXT trading day

--- signal/unlistings/unlistings.go
+++ signal/unlistings/unlistings.go
@@ -4,44 +4,52 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/Clever/go-utils/stringset"
 	"github.com/d-sparks/gravy/db"
 	"github.com/d-sparks/gravy/db/dailyprices"
 	"github.com/d-sparks/gravy/signal"
 )
 
+// Name of the unlistings signal.
 const Name = "unlistings"
 
-// Unlistings reports new values since last tick.
-type Unlistings struct {
-	previousTickers stringset.StringSet
-}
+// Unlistings reports tickers that will not be listed tomorrow.
+type Unlistings struct{}
 
+// New cached Unlistings.
 func New() *signal.CachedSignal {
-	return signal.NewCachedSignal(
-		&Unlistings{previousTickers: stringset.StringSet{}},
-		time.Hour*24*365,
-	)
+	return signal.NewCachedSignal(&Unlistings{}, time.Hour*24*365)
 }
 
+// Name returns the name of Unlistings as per the signal interface.
 func (u *Unlistings) Name() string {
 	return Name
 }
 
+// Compute computes the signal as per the signal interface.
 func (u *Unlistings) Compute(date time.Time, stores map[string]db.Store) (*signal.SignalOutput, error) {
-	data, err := stores[dailyprices.Name].Get(date)
+	todayData, err := stores[dailyprices.Name].Get(date)
 	if err != nil {
-		return nil, fmt.Errorf("Error reading dailyprices in `%s`: `%s`", Name, err.Error())
+		return nil, fmt.Errorf("Error reading todayprices in `%s`: `%s`", Name, err.Error())
 	}
-	output := signal.SignalOutput{StringSet: u.previousTickers.Minus(data.Tickers)}
-	u.previousTickers = data.Tickers
-	return &output, nil
+
+	tomorrowDate, err := stores[dailyprices.Name].NextDate(date)
+	if err != nil {
+		return nil, fmt.Errorf("Error getting next date in `%s`: `%s`", Name, err.Error())
+	}
+
+	tomorrowData, err := stores[dailyprices.Name].Get(*tomorrowDate)
+	if err != nil {
+		return nil, fmt.Errorf("Error getting tomorrowprices in `%s`: `%s`", Name, err.Error())
+	}
+	return &signal.SignalOutput{StringSet: todayData.Tickers.Minus(tomorrowData.Tickers)}, nil
 }
 
+// Headers as per signal interface.
 func (u *Unlistings) Headers() []string {
 	return []string{}
 }
 
+// Debug as per signal interface.
 func (u *Unlistings) Debug() map[string]string {
 	return map[string]string{}
 }
